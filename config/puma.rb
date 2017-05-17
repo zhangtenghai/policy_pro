767f85be345ecf39b07a932d6c73c3ce2969bf85
bruce <zth5906@gmail.com>
2017-05-17 18:20:17 +0800

    merge

--- config/puma.rb
+++ config/puma.rb
@@ -4,17 +4,17 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+workers 1
 
+# Min and Max threads per worker
+threads 1, 6
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
-
+environment ENV.fetch("RAILS_ENV") { "production" }
+preload_app!
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
@@ -44,4 +44,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# plugin :tmp_restart
