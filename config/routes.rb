44c718bb7f1cdd8a4044a97195abe0d96704e7a0
bruce <zth5906@gmail.com>
2017-05-18 16:17:32 +0800

    i18n

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     end 
    
   end
-  root "users#index"
+  root "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
